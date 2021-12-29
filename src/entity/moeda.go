10763522ffc1bda142291676e309d908d9da995d
raianeeunice <liraradasa@gmail.com>
2021-12-29 12:14:38 -0300

    alterando o modelo para que a sigla seja a primaryKey

--- src/entity/moeda.go
+++ src/entity/moeda.go
@@ -2,6 +2,6 @@ package entity
 
 type Moeda struct {
 	Sigla     string  `json:"sigla" gorm:"primaryKey"`
-	Descricao string  `json:"descricao" gorm:"unique"`
-	Cotacao   float64 `json:"cotacao"`
+	Descricao string  `json:"descricao" gorm:"not null"`
+	Cotacao   float64 `json:"cotacao" gorm:"not null"`
 }
